b3c9a53ba7d8a49d4255dc3f28682a951cf55f42
m.mernissi <m.mernissi@revotrends.com>
2022-08-30 09:32:48 +0100

    Initial Commit

---
+++ Guidomia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by mohamed mernissi on 30/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
